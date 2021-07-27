d885a0f45a33f31a3f98cf757d0aa29c0c33a739
JukangPark <skyxxx9339@naver.com>
2021-07-27 18:43:40 +0900

    07/27

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,35 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", potato: "tomato"}); // Comes from your controller
+// 내가 만든 객체를 
+// 우리의 template에 보낼 것입니다.
+export const trending = (req, res) => {
+    const videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1, 
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1, 
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1, 
+    },
+    
+];
+    return res.render("home", { pageTitle: "Home", videos});
+}
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
