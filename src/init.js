99002415185200a7518c3c775c30fd8bfabe0e11
jukangpark <skyxxx9339@naver.com>
2021-10-26 14:27:12 +0900

    comments

--- src/init.js
+++ src/init.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import "./db"; // 파일 자체를 import 해주기 서버는 이 라인을 보는순간 이 파일을 import 해줌으로써 서버가 mongo에 연결될거야.
 import "./models/Video";
 import "./models/User";
+import "./models/Comment";
 import app from "./server";
 
 const PORT = 9000;
