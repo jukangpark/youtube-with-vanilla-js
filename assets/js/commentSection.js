4750cc16a2d13d06c4c6bc03fb7eddeb34d9b704
jukangpark <skyxxx9339@naver.com>
2021-10-27 17:38:35 +0900

    comment section

--- assets/js/commentSection.js
+++ assets/js/commentSection.js
@@ -15,7 +15,7 @@
   \*****************************************/
 /***/ (() => {
 
-eval("var videoContainer = document.getElementById(\"videoContainer\");\nvar form = document.getElementById(\"commentForm\");\n\nvar handleSubmit = function handleSubmit(event) {\n  event.preventDefault();\n  var textarea = form.querySelector(\"textarea\");\n  var text = textarea.value;\n  var videoId = videoContainer.dataset.id;\n  fetch(\"/api/videos/\".concat(videoId, \"/comment\"), {\n    method: \"POST\",\n    body: {\n      text: text\n    }\n  });\n};\n\nif (form) {\n  form.addEventListener(\"submit\", handleSubmit);\n}\n\n//# sourceURL=webpack://Youtube_Clone/./src/client/js/commentSection.js?");
+eval("var videoContainer = document.getElementById(\"videoContainer\");\nvar form = document.getElementById(\"commentForm\");\n\nvar handleSubmit = function handleSubmit(event) {\n  event.preventDefault();\n  var textarea = form.querySelector(\"textarea\");\n  var text = textarea.value;\n  var videoId = videoContainer.dataset.id;\n\n  if (text === \"\") {\n    return;\n  }\n\n  fetch(\"/api/videos/\".concat(videoId, \"/comment\"), {\n    method: \"POST\",\n    headers: {\n      \"Content-Type\": \"application/json\"\n    },\n    body: JSON.stringify({\n      text: text\n    })\n  });\n};\n\nif (form) {\n  form.addEventListener(\"submit\", handleSubmit);\n}\n\n//# sourceURL=webpack://Youtube_Clone/./src/client/js/commentSection.js?");
 
 /***/ })
 
