677a30d2ad86d53b4e68e52df16e2099ec104e39
jukangpark <skyxxx9339@naver.com>
2021-11-01 12:38:07 +0900

    asdfas

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,18 @@
 const multer = require("multer");
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "youtubejukangpark",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -35,6 +49,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
 
 export const videoUpload = multer({
@@ -42,4 +57,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
